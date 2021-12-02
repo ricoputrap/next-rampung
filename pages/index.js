5ec7b8969ce8149fd91da6dd1de483b44e106f68
Rico Putra Pradana <rico.putra@outlook.co.id>
2021-12-02 22:35:16 +0700

    fix crossOrigin value in Head

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home({ projects }) {
         <meta name="keywords" content="task management, project management, drag and drop" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet"></link>
       </Head>
       
